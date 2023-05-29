d1eb462e5143558a030bcdb80444f89af3f4f7a6
Burak Oz <oz.burak1995@gmail.com>
2023-05-29 02:32:17 +0200

    2023 updates

--- truffle-config.js
+++ truffle-config.js
@@ -8,24 +8,24 @@ module.exports = {
       port: 7545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
-    rinkeby: {
+    sepolia: {
       provider: function () {
         return new HDWalletProvider({
           privateKeys: [process.env.PRIVATE_KEY_1],
-          providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+          providerOrUrl: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
           numberOfAddresses: 1,
         });
       },
       gas: 5000000,
       gasPrice: 5000000000, // 5 gwei
-      network_id: 4,
+      network_id: 11155111,
     },
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.11", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.15", // Fetch exact version from solc-bin (default: truffle's version)
     },
   },
 };
